8c34b69975e04f1189a56f79dd28a374204c00bf
Hank Tom <hanktom@gmail.com>
2016-08-21 16:39:38 +0800

    setValue/push

--- app/src/main/java/com/tom/fire9/MainActivity.java
+++ app/src/main/java/com/tom/fire9/MainActivity.java
@@ -3,6 +3,7 @@ package com.tom.fire9;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,10 +12,51 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if (user !=null) {
+            String uid = user.getUid();
+            Log.d("MainActivity", "UID:"+uid);
+            FirebaseDatabase db = FirebaseDatabase.getInstance();
+            DatabaseReference users = db.getReference("users");
+            users.child(uid).child("phone").setValue("098887766");
+            users.child(uid).child("nickname")
+                    .setValue("Jack");
+            DatabaseReference friendRef = users
+                    .child(uid)
+                    .child("friends").push();
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", "Mary");
+            map.put("phone", "88776688");
+            friendRef.setValue(map);
+
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (user!=null){
+            auth.signOut();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
         if (FirebaseAuth.getInstance().getCurrentUser()==null){
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
+        }else{
+
         }
 
 //        Firebase.setAndroidContext(this);
